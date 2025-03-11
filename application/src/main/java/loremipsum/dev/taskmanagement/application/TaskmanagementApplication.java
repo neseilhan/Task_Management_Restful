cb34da229f9df163c354c72e8e87a7b2a48bf4a2
Nese <neseilhannn@gmail.com>
2025-03-11 22:18:29 +0300

    appConfig and properties added.

--- application/src/main/java/loremipsum/dev/taskmanagement/application/TaskmanagementApplication.java
+++ application/src/main/java/loremipsum/dev/taskmanagement/application/TaskmanagementApplication.java
@@ -1,4 +1,4 @@
-package loremipsum.dev.taskmanagement;
+package loremipsum.dev.taskmanagement.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ application/src/main/java/loremipsum/dev/taskmanagement/application/config/AppConfig.java
@@ -0,0 +1,20 @@
+package loremipsum.dev.taskmanagement.application.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackages = "loremipsum.dev.taskmanagement.entities")
+@EnableJpaRepositories(basePackages = {
+        "loremipsum.dev.taskmanagement.repositories"
+})
+@ComponentScan(basePackages = {
+        "loremipsum.dev.taskmanagement.services",
+        "loremipsum.dev.taskmanagement.controllers",
+        "loremipsum.dev.taskmanagement.application"}
+)
+public class AppConfig {
+
+}
