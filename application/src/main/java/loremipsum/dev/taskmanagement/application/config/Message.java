7f59d46b594921ed6504d2209c3018a24f81a168
Nese <neseilhannn@gmail.com>
2025-03-12 04:06:25 +0300

    result helper

---
+++ application/src/main/java/loremipsum/dev/taskmanagement/application/config/Message.java
@@ -0,0 +1,9 @@
+package loremipsum.dev.taskmanagement.application.config;
+
+public class Message {
+    public static final String CREATED = "Data successfully saved.";
+    public static final String OK = "Process successfully executed.";
+    public static final String NOT_FOUND = "Data not found.";
+    public static final String RECORD_NOT_FOUND_WITH_ID = "Record not found in the system with id: ";
+    public static final String RECORD_ALREADY_EXISTS = "Record already exists in the system with id: ";
+}
