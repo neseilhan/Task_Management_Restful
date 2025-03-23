cad4f129086c774fa43aa1941de02cb84a5bf7fd
Nese <neseilhannn@gmail.com>
2025-03-23 22:47:59 +0300

    the last working version of the project

--- application/src/main/java/loremipsum/dev/taskmanagement/TaskmanagementApplication.java
+++ application/src/main/java/loremipsum/dev/taskmanagement/TaskmanagementApplication.java
@@ -11,12 +11,6 @@ public class TaskmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagementApplication.class, args);
-//
-//		byte[] key = new byte[32];
-//		new SecureRandom().nextBytes(key);
-//		String base64Key = Base64.getEncoder().encodeToString(key);
-//
-//		System.out.println("Base64 Encoded Secret Key: " + base64Key);
 
 	}
 
